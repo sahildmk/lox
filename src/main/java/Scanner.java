342bfde603f9227622fb8bf809c0b42c4b3756c6
sahildmk <sahildmk@gmail.com>
2025-02-13 17:34:46 +1100

    added 2 char tokens + comments + strings

--- src/main/java/Scanner.java
+++ src/main/java/Scanner.java
@@ -1,7 +1,9 @@
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Scanner {
+
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
 
@@ -14,7 +16,7 @@ public class Scanner {
     // Source line number for lexeme
     private int line = 1;
 
-    Scanner (String source) {
+    Scanner(String source) {
         this.source = source;
     }
 
@@ -36,19 +38,57 @@ public class Scanner {
         char c = advance();
 
         switch (c) {
-            case '(': addToken(TokenType.LEFT_PAREN); break;
-            case ')': addToken(TokenType.RIGHT_PAREN); break;
-            case '{': addToken(TokenType.LEFT_BRACE); break;
-            case '}': addToken(TokenType.RIGHT_BRACE); break;
-            case ',': addToken(TokenType.COMMA); break;
-            case '.': addToken(TokenType.DOT); break;
-            case '-': addToken(TokenType.MINUS); break;
-            case '+': addToken(TokenType.PLUS); break;
-            case ';': addToken(TokenType.SEMICOLON); break;
-            case '*': addToken(TokenType.STAR); break;
+            case '(' ->
+                addToken(TokenType.LEFT_PAREN);
+            case ')' ->
+                addToken(TokenType.RIGHT_PAREN);
+            case '{' ->
+                addToken(TokenType.LEFT_BRACE);
+            case '}' ->
+                addToken(TokenType.RIGHT_BRACE);
+            case ',' ->
+                addToken(TokenType.COMMA);
+            case '.' ->
+                addToken(TokenType.DOT);
+            case '-' ->
+                addToken(TokenType.MINUS);
+            case '+' ->
+                addToken(TokenType.PLUS);
+            case ';' ->
+                addToken(TokenType.SEMICOLON);
+            case '*' ->
+                addToken(TokenType.STAR);
+            case '!' ->
+                addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+            case '=' ->
+                addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+            case '<' ->
+                addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+            case '>' ->
+                addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+            case '/' -> {
+                if (match('/')) {
+                    // Comment continues till the end of the line
+                    while (isPeekingChar('\n')) {
+                        advance();
+                    }
+                } else {
+                    addToken(TokenType.SLASH);
+                }
+            }
+            case ' ', '\r', '\t' -> {
+            }
+            case '\n' ->
+                line++;
+            case '"' ->
+                string();
+            default ->
+                Lox.error(line, "Unexpected character.");
         }
+        // Ignore whitespace.
     }
 
+    // Returns next character
     private char advance() {
         return source.charAt(current++);
     }
@@ -60,5 +100,50 @@ public class Scanner {
     private void addToken(TokenType type, Object literal) {
         String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) {
+            return false;
+        }
+        if (source.charAt(current) != expected) {
+            return false;
+        }
+
+        current++;
+        return true;
+    }
+
+    private char peek() {
+        if (isAtEnd()) {
+            return '\0';
+        }
+
+        return source.charAt(current);
+    }
+
+    private boolean isPeekingChar(char c) {
+        return peek() != c && !isAtEnd();
+    }
+
+    private void string() {
+        while (isPeekingChar('"')) {
+            if (peek() == '\n') {
+                line++;
+            }
+
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        // Reach the closing "
+        advance();
+
+        String value = source.substring(start + 1, current - 1);
+        addToken(TokenType.STRING, value);
     }
 }
